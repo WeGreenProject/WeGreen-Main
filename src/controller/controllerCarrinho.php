71c5675894e88e1ad965dccaf888d41c6e59faa2
jessicaisabelf <jessicaisabelcfernandes2001@gmail.com>
2025-11-23 20:55:56 +0000

    Carrinho
    Carrinho

---
+++ src/controller/controllerCarrinho.php
@@ -0,0 +1,42 @@
+<?php
+include_once '../model/modelCarrinho.php';
+
+$func = new Carrinho();
+
+if ($_POST['op'] == 1) {
+    $resp = $func->getCarrinho();
+    echo $resp;
+}
+
+if ($_POST['op'] == 2) {
+    $resp = $func->getResumoPedido(); 
+    echo $resp;
+}
+
+if ($_POST['op'] == 3) {
+    $produto_id = $_POST['produto_id'];
+    $mudanca = $_POST['mudanca'];
+    $resp = $func->atualizarQuantidade($produto_id, $mudanca);
+    echo $resp;
+}
+
+if ($_POST['op'] == 4) {
+    $produto_id = $_POST['produto_id'];
+    $resp = $func->removerDoCarrinho($produto_id);
+    echo $resp;
+}
+
+if ($_POST['op'] == 5) {
+    $resp = $func->limparCarrinho();
+    echo $resp;
+}
+
+if ($_POST['op'] == 6) {
+    $codigo = $_POST['codigo'];
+    if ($codigo == 'WEGREEN10') {
+        echo "Cupão aplicado com sucesso! Desconto de 10%.";
+    } else {
+        echo "Cupão inválido ou expirado.";
+    }
+}
+?>
