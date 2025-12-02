12b95dd64486e75e09b8c36a1512b25288857cb1
jessicaisabelf <jessicaisabelcfernandes2001@gmail.com>
2025-12-02 20:28:38 +0000

    DA
    Designer / Artesão

--- src/controller/controllerDesigner.php
+++ src/controller/controllerDesigner.php
@@ -3,13 +3,25 @@ include_once '../model/modelDesigner.php';
 session_start();
 
 $func = new Designer();
-
 if ($_POST['op'] == 1) {
-    $resp = $func->getProdutosDesigner();
+    $resp = $func->getFiltrosDesignerCategoria();
     echo $resp;
 }
+
 if ($_POST['op'] == 2) {
+    $resp = $func->getProdutosDesigner($_POST["categoria"],$_POST["tamanho"],$_POST["estado"]);
+    echo $resp;
+}
+if ($_POST['op'] == 3) {
     $resp = $func->getProdutoDesignerMostrar($_POST["id"]);
+    echo $resp;
+}
+if ($_POST['op'] == 7) {
+    $resp = $func->getFiltrosDesignerTamanho();
+    echo $resp;
+}
+if ($_POST['op'] == 8) {
+    $resp = $func->getFiltrosDesignerEstado();
     echo $resp;
 }
 ?>
--- src/js/designer.js
+++ src/js/designer.js
@@ -1,8 +1,93 @@
-function getProdutosDesigner()
+function getFiltrosDesignerCategoria()
 {
     let dados = new FormData();
-    dados.append("op", 1);
+    dados.append("op",1);
 
+    $.ajax({
+    url: "src/controller/controllerDesigner.php",
+    method: "POST",
+    data: dados,
+    dataType: "html",
+    cache: false,
+    contentType: false,
+    processData: false
+    })
+    .done(function( msg ) {
+        console.log(msg);
+        $('#CategoriaSelect').html(msg);
+
+    })
+    
+    .fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+    });
+}
+function getFiltrosDesignerTamanho()
+{
+    let dados = new FormData();
+    dados.append("op",7);
+
+    $.ajax({
+    url: "src/controller/controllerDesigner.php",
+    method: "POST",
+    data: dados,
+    dataType: "html",
+    cache: false,
+    contentType: false,
+    processData: false
+    })
+    .done(function( msg ) {
+        console.log(msg);
+        $('#tamanhoSelect').html(msg);
+
+    })
+    
+    .fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+    });
+}
+function getFiltrosDesignerEstado()
+{
+    let dados = new FormData();
+    dados.append("op",8);
+
+    $.ajax({
+    url: "src/controller/controllerDesigner.php",
+    method: "POST",
+    data: dados,
+    dataType: "html",
+    cache: false,
+    contentType: false,
+    processData: false
+    })
+    .done(function( msg ) {
+        console.log(msg);
+        $('#estadoSelect').html(msg);
+
+
+    })
+    
+    .fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+    });
+}
+function getFiltrosLimparFiltro() {
+    $("#CategoriaSelect").val('-1');
+    $("#tamanhoSelect").val('-1');
+    $("#estadoSelect").val('-1');
+    getProdutosDesigner();
+}
+function getProdutosDesigner()
+{
+    let categoria = $("#CategoriaSelect").val();
+    let tamanho = $("#tamanhoSelect").val();
+    let estado = $("#estadoSelect").val();
+
+    let dados = new FormData();
+    dados.append("op", 2);
+    dados.append("categoria", categoria);
+    dados.append("tamanho", tamanho);
+    dados.append("estado", estado);
     $.ajax({
     url: "src/controller/controllerDesigner.php",
     method: "POST",
@@ -15,6 +100,7 @@ function getProdutosDesigner()
     
     .done(function( msg ) {
          $('#ProdutoDesignerVenda').html(msg);
+
     })
     
     .fail(function( jqXHR, textStatus ) {
@@ -22,3 +108,75 @@ function getProdutosDesigner()
     });
 
 }
+
+function getProdutoDesignerMostrar()
+{
+    const params = new URLSearchParams(window.location.search);
+    const produtoID = params.get("id");
+
+    let dados = new FormData();
+    dados.append("op", 3);
+    dados.append("id", produtoID);
+
+    $.ajax({
+    url: "src/controller/controllerDesigner.php",
+    method: "POST",
+    data: dados,
+    dataType: "html",
+    cache: false,
+    contentType: false,
+    processData: false
+    })
+    
+    .done(function( msg ) {
+        console.log(msg);
+         $('#ProdutoInfo').html(msg);
+
+         $('.btnComprarAgora').on('click', function() {
+            const produtoId = $(this).data('id');
+            comprarAgora(produtoId);
+        });
+    })
+    
+    .fail(function( jqXHR, textStatus ) {
+    alert( "Request failed: " + textStatus );
+    });
+
+}
+
+function comprarAgora(produtoId) {
+
+    let dados = new FormData();
+    dados.append("op", 7);
+    dados.append("produto_id", produtoId);
+
+    $.ajax({
+        url: "src/controller/controllerCarrinho.php",
+        method: "POST",
+        data: dados,
+        contentType: false,
+        processData: false
+    })
+       .done(function() {
+
+        Swal.fire({
+            title: 'Sucesso!',
+            text: 'Produto adicionado ao carrinho',
+            icon: 'success',
+            confirmButtonColor: '#28a745',
+            confirmButtonText: 'OK'
+        })
+        });
+
+}
+
+$(function() {
+    getProdutoDesignerMostrar();
+    getProdutosDesigner();
+    getFiltrosDesignerCategoria();
+    getFiltrosDesignerTamanho();
+    getFiltrosDesignerEstado();
+     $("#CategoriaSelect, #tamanhoSelect, #estadoSelect").on('change', function() {
+        getProdutosDesigner();
+    });
+});
